f5c7d88c38536751180c80354ecfc9d58699bd9a
Arthur Drabazha <72470924+pikulo-kama@users.noreply.github.com>
2023-10-28 03:03:40 +0300

    [v1.2] Make popups design consistent with main app design

--- gui/confirmation.py
+++ gui/confirmation.py
@@ -1,7 +1,10 @@
+import os
 import tkinter as tk
 
 from constants import CONFIRMATION_WINDOW_CONFIRM_BTN, CONFIRMATION_WINDOW_CLOSE_BTN, CONFIRMATION_WINDOW_WIDTH, \
-    CONFIRMATION_WINDOW_HEIGHT, CONFIRMATION_WINDOW_TITLE
+    CONFIRMATION_WINDOW_HEIGHT, CONFIRMATION_WINDOW_TITLE, CONFIRMATION_ICO, PROJECT_ROOT, NOTIFICATION_TEXT_COLOR, \
+    CONFIRMATION_TEXT_COLOR, CONFIRMATION_CONFIRM_BTN_PROPERTIES, CONFIRMATION_CANCEL_BTN_PROPERTIES, \
+    CONFIRMATION_POPUP_PROPERTY_LIST
 
 
 class Confirmation:
@@ -11,17 +14,30 @@ class Confirmation:
 
     def show_notification(self, message, callback):
 
+        def on_button_leave(e):
+            color_mapping = {btn["colorHover"]: btn["colorStatic"] for btn in CONFIRMATION_POPUP_PROPERTY_LIST}
+            e.widget['bg'] = color_mapping[e.widget['bg']]
+
+        def on_button_enter(e):
+            color_mapping = {btn["colorStatic"]: btn["colorHover"] for btn in CONFIRMATION_POPUP_PROPERTY_LIST}
+            e.widget['bg'] = color_mapping[e.widget['bg']]
+
         self.__window.geometry(f"{CONFIRMATION_WINDOW_WIDTH}x{CONFIRMATION_WINDOW_HEIGHT}")
         self.__window.title(CONFIRMATION_WINDOW_TITLE)
+        self.__window.iconbitmap(os.path.join(PROJECT_ROOT, CONFIRMATION_ICO))
         self.__window.resizable(False, False)
 
-        container = tk.Frame(self.__window, pady=40)
+        container = tk.Frame(self.__window)
         container.place(relx=.5, rely=.5, anchor=tk.CENTER)
 
-        message_label = tk.Label(container, text=message)
-        message_label.grid(row=0, column=0)
+        message_label = tk.Label(
+            container,
+            text=message,
+            fg=CONFIRMATION_TEXT_COLOR,
+            font=('Helvetica', 10, 'bold')
+        )
 
-        button_frame = tk.Frame(self.__window)
+        button_frame = tk.Frame(container)
 
         confirm_btn = tk.Button(
             button_frame,
@@ -37,9 +53,39 @@ class Confirmation:
             command=lambda: self.__window.destroy()
         )
 
-        button_frame.place(relx=.5, rely=.7, anchor=tk.N)
+        confirm_btn.config(
+            fg=CONFIRMATION_TEXT_COLOR,
+            bg=CONFIRMATION_CONFIRM_BTN_PROPERTIES["colorStatic"],
+            borderwidth=0,
+            relief=tk.SOLID,
+            pady=5,
+            padx=5,
+            font=4
+        )
+
+        close_btn.config(
+            fg=CONFIRMATION_TEXT_COLOR,
+            bg=CONFIRMATION_CANCEL_BTN_PROPERTIES["colorStatic"],
+            borderwidth=0,
+            relief=tk.SOLID,
+            pady=5,
+            padx=5,
+            font=4
+        )
+
+        confirm_btn.bind('<Enter>', on_button_enter)
+        confirm_btn.bind('<Leave>', on_button_leave)
+
+        close_btn.bind('<Enter>', on_button_enter)
+        close_btn.bind('<Leave>', on_button_leave)
+
         confirm_btn.grid(row=0, column=0, padx=20)
         close_btn.grid(row=0, column=1)
+
+        message_label.grid(row=0, column=0, pady=20)
+        button_frame.grid(row=1, column=0)
+
+        container.place(relx=.5, rely=.5)
 
         self.__window.mainloop()
 
--- gui/notification.py
+++ gui/notification.py
@@ -1,7 +1,9 @@
+import os
 import tkinter as tk
 
 from constants import NOTIFICATION_WINDOW_TITLE, NOTIFICATION_WINDOW_WIDTH, NOTIFICATION_WINDOW_HEIGHT, \
-    NOTIFICATION_WINDOW_CLOSE_BTN
+    NOTIFICATION_WINDOW_CLOSE_BTN, PROJECT_ROOT, NOTIFICATION_ICO, NOTIFICATION_TEXT_COLOR, \
+    NOTIFICATION_POPUP_PROPERTY_LIST, NOTIFICATION_CLOSE_BTN_PROPERTIES
 
 
 class Notification:
@@ -10,23 +12,53 @@ class Notification:
         self.__window = tk.Tk()
 
     def show_notification(self, message):
+
+        def on_button_leave(e):
+            color_mapping = {btn["colorHover"]: btn["colorStatic"] for btn in NOTIFICATION_POPUP_PROPERTY_LIST}
+            e.widget['bg'] = color_mapping[e.widget['bg']]
+
+        def on_button_enter(e):
+            color_mapping = {btn["colorStatic"]: btn["colorHover"] for btn in NOTIFICATION_POPUP_PROPERTY_LIST}
+            e.widget['bg'] = color_mapping[e.widget['bg']]
+
         self.__window.geometry(f"{NOTIFICATION_WINDOW_WIDTH}x{NOTIFICATION_WINDOW_HEIGHT}")
         self.__window.title(NOTIFICATION_WINDOW_TITLE)
+        self.__window.iconbitmap(os.path.join(PROJECT_ROOT, NOTIFICATION_ICO))
         self.__window.resizable(False, False)
 
-        container = tk.Frame(self.__window, pady=40)
-        container.place(relx=.5, rely=.5, anchor=tk.CENTER)
+        container = tk.Frame(self.__window)
 
-        message_label = tk.Label(container, text=message)
-        message_label.grid(row=0, column=0)
+        message_label = tk.Label(
+            container,
+            text=message,
+            fg=NOTIFICATION_TEXT_COLOR,
+            font=('Helvetica', 10, 'bold')
+        )
 
         close_btn = tk.Button(
             container,
             text=NOTIFICATION_WINDOW_CLOSE_BTN,
-            width=40,
+            width=20,
             command=lambda: self.__window.destroy()
         )
+
+        close_btn.config(
+            fg=NOTIFICATION_TEXT_COLOR,
+            bg=NOTIFICATION_CLOSE_BTN_PROPERTIES["colorStatic"],
+            borderwidth=0,
+            relief=tk.SOLID,
+            pady=5,
+            padx=5,
+            font=4
+        )
+
+        close_btn.bind('<Enter>', on_button_enter)
+        close_btn.bind('<Leave>', on_button_leave)
+
+        message_label.grid(row=0, column=0, pady=20)
         close_btn.grid(row=1, column=0)
+
+        container.place(relx=.5, rely=.5, anchor=tk.CENTER)
 
         self.__window.mainloop()
 
