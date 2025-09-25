c2fbc8dbbad116dab8e329e422e45a201e48660b
Artur Drabazha <jhorr2222@gmail.com>
2025-09-26 00:59:40 +0300

    Add game change worker

---
+++ savegem/app/gui/worker/game_change_worker.py
@@ -0,0 +1,19 @@
+from savegem.app.gui.worker import QWorker
+from savegem.common.core import app
+
+
+class GameChangeWorker(QWorker):
+    """
+    Worker used to change currently selected.
+    """
+
+    def __init__(self, new_game):
+        super().__init__()
+        self.__new_game = new_game
+
+    def _run(self):
+        app.state.game_name = self.__new_game
+        app.games.current.meta.drive.refresh()
+        app.activity.refresh()
+
+        self.completed.emit()
