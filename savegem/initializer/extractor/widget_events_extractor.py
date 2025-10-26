520f67d4712d04d57d7ab13f7407698eb6c65a90
Artur Drabazha <jhorr2222@gmail.com>
2025-10-26 03:06:09 +0300

    Update widget events import/extract by including refresh_children flag

--- savegem/initializer/extractor/widget_events_extractor.py
+++ savegem/initializer/extractor/widget_events_extractor.py
@@ -8,7 +8,7 @@ class WidgetEventsExtractor(RegularExtractor):
 
     def _post_extract(self, data: list[dict]):
 
-        formatted_data = {}
+        formatted_data: dict[str, dict] = {}
 
         for record in data:
 
@@ -17,9 +17,22 @@ class WidgetEventsExtractor(RegularExtractor):
             event = record.get("refresh_event_id")
             widget_name = f"{section_id}.{widget_id}"
 
-            events = formatted_data.get(widget_name, [])
+            widget_obj = formatted_data.get(widget_name)
+
+            if widget_obj is None:
+                widget_obj = {
+                    "refresh_children": record.get("refresh_children") == 1,
+                    "events": []
+                }
+
+                # Only include refresh children flag if it's True.
+                if not widget_obj.get("refresh_children"):
+                    del widget_obj["refresh_children"]
+
+            events = widget_obj.get("events")
             events.append(event)
 
-            formatted_data[widget_name] = events
+            widget_obj["events"] = events
+            formatted_data[widget_name] = widget_obj
 
         return formatted_data
--- savegem/initializer/importer/widget_events_importer.py
+++ savegem/initializer/importer/widget_events_importer.py
@@ -6,17 +6,20 @@ class WidgetEventsImporter(RegularImporter):
     Importer for ui_widget_events table.
     """
 
-    def _format_data(self, data: dict[str, list[str]]):
+    def _format_data(self, data: dict[str, dict]):
         formatted_data = []
 
-        for widget_name, events in data.items():
+        for widget_name, widget_data in data.items():
             section_id, widget_id = widget_name.split(".")
+            refresh_children = widget_data.get("refresh_children")
+            events = widget_data.get("events")
 
             for event in events:
                 formatted_data.append({
                     "widget_id": widget_id,
                     "section_id": section_id,
-                    "refresh_event_id": event
+                    "refresh_event_id": event,
+                    "refresh_children": 1 if refresh_children else 0
                 })
 
         return formatted_data
