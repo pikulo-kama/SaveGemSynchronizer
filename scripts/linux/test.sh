2b7f146c4dd57188de54e943d7916c4d9b3f62be
Artur Drabazha <jhorr2222@gmail.com>
2025-11-28 02:12:37 +0200

    Update test shell script

--- scripts/linux/test.sh
+++ scripts/linux/test.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
 
+# Import helper methods.
+source ./scripts/linux/util.sh
+
 sub_package=$1
+test_name=$2
 
-source_package="savegem"
-tests_path="tests"
+source_package_list=("savegem")
+tests_path_list=("tests")
 
-if [ -n "$sub_package" ]; then
-  source_package="${source_package}.${sub_package}"
-  tests_path="${tests_path}/${sub_package//./\/}"
+for sub_package_part in $(string_split "$sub_package" "."); do
+
+    # If test file provided directly it shouldn't be
+    # included in package name.
+    if [[ "$sub_package_part" == "test_"* ]]; then
+      tests_path_list+=("$sub_package_part.py")
+      continue
+    fi
+
+    source_package_list+=("$sub_package_part")
+    tests_path_list+=("$sub_package_part")
+done
+
+source_package=$(array_join source_package_list ".")
+tests_path=$(array_join tests_path_list "/")
+
+if [ -n "$test_name" ]; then
+  tests_path="$tests_path::$test_name"
 fi
 
 echo "Source package: $source_package"
--- scripts/linux/util.sh
+++ scripts/linux/util.sh
@@ -10,3 +10,37 @@ verify_file_exists() {
         exit 1
     fi
 }
+
+
+# array_join: Used to join array elements into
+# single string using provided delimiter.
+# @param array - list with elements that needs to be joined
+# @param delimiter - string delimiter that would be used to join array
+# Usage: array_join ["1", "2"] ", " -> "12"
+array_join() {
+  declare -n array="$1"
+  local delimiter="$2"
+
+  local old_ifs=$IFS
+  IFS="$delimiter"
+
+  local joined_array="${array[*]}"
+  IFS=$old_ifs
+
+  echo "$joined_array"
+}
+
+
+# string_split: Used to split string using separator into
+# iterable object.
+# @param string - string to split into array
+# @param delimiter - string that should be used as delimiter when splitting
+# Usage: string_split "test:123" ":" -> ["test", "123"]
+string_split() {
+    local string="$1"
+    local delimiter="$2"
+
+    IFS="$delimiter" read -r -a parts <<< "$string"
+
+    printf "%s\n" "${parts[@]}"
+}
